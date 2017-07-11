804291b70f7926642cd971738bd47b5103469d53
TimG <timoguic@gmail.com>
2017-07-11 23:43:03 +0200

    Added abstract class and user dependency

--- sp_app/models.py
+++ sp_app/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
-class Conversation(models.Model):
+class SpObject(models.Model):
     title = models.CharField(max_length=200, null=True, blank=False)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, null=True, blank=False)
     published_date = models.DateTimeField('published date')
 
-class Article(models.Model):
-    title = models.CharField(max_length=200, null=True, blank=False)
-    published_date = models.DateTimeField('published_date')
+    class Meta:
+        abstract = True
+
+class Conversation(SpObject):
+    pass
+
+class Article(SpObject):
+    pass
