3e0bdb685dbaec8576e16361d9a026097a4b1270
TimG <timoguic@gmail.com>
2017-07-12 23:51:55 +0200

    Added XQ file for BaseX reference

--- sp_app/views.py
+++ sp_app/views.py
@@ -1,33 +1,3 @@
-"""
-declare
-  %rest:path("/sph/tim/titles")
-  %rest:GET
-  %output:method("json")
-  function page:getTitlesTim() {
-    let $sph := db:open("sph")//*:html
-    return <json type='array'> {
-    for $title in $sph/head/meta[@name="DC.title"]
-      return <_ type='object'><name>{fn:data($title/@content)}</name></_>
-    }</json>
-
-  };
-
-declare
-  %rest:path("/sph/tim/authors")
-  %rest:GET
-  %output:method("json")
-  function page:getAuthorsTim() {
-    let $authors := db:open("sph")//meta[@name="DC.creator"]
-    let $authors := fn:distinct-values($authors/@content)
-
-    return <json type='array'> {
-      for $author in $authors
-        return <_ type='object'><name>{$author}</name></_>
-    }</json>
-  };
-
-"""
-
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
