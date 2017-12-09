d35a36e256d68ae79c62f5712db47d1ffeb2b250
TimG <timoguic@gmail.com>
2017-12-09 16:34:15 +0100

    update

--- drf_sp_hub/sp_hub/settings.py
+++ drf_sp_hub/sp_hub/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'sp_hub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'database',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'sp_db',
