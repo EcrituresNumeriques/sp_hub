bbadeaddd933d9c537648f904684231c4345dae3
TimG <timoguic@gmail.com>
2017-12-09 20:26:48 +0100

    test commit deployment

--- drf_sp_hub/sp_hub/settings.py
+++ drf_sp_hub/sp_hub/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'sp_hub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sp_hub',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'sp_db',
