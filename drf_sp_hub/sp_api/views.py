1f8530f529651b781a370f223ce12791ab1a2039
TimG <timoguic@gmail.com>
2018-01-31 21:09:37 +0100

    Ajustement des routes mots clés pour l'API

--- drf_sp_hub/sp_api/views.py
+++ drf_sp_hub/sp_api/views.py
@@ -29,13 +29,13 @@ class SPKeywordViewSet(viewsets.ModelViewSet):
     serializer_class = SPKeywordSerializer
 
     @list_route()
-    def list_editor(self, request):
+    def editor(self, request):
         queryset = SPKeyword.objects.filter(aligned=True)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
     @list_route()
-    def list_author(self, request):
+    def author(self, request):
         queryset = SPKeyword.objects.filter(aligned=False)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
